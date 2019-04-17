0f2e274a6ab04b0a821e193a0049722dfb031f47
bupalem <pavan.bupalem@gmail.com>
2019-04-17 22:32:50 +0530

    I added new class for current project

---
+++ MavenDemoTest/src/test/java/sample/GitClass.java
@@ -0,0 +1,10 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class GitClass {
+  @Test
+  public void f() {
+	  System.out.println("Created the new local repository and commit the code");
+  }
+}
