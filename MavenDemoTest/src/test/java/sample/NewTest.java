3082c1d3f5a15fc0a2f152237ffe1c597ea481db
bupalem <pavan.bupalem@gmail.com>
2019-04-26 21:48:08 +0530

    final commit

--- MavenDemoTest/src/test/java/sample/NewTest.java
+++ MavenDemoTest/src/test/java/sample/NewTest.java
@@ -1,23 +1,30 @@
 package sample;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.log4testng.Logger;
 
 public class NewTest {
-	Logger log=Logger.getLogger(NewTest.class);
+	
   @Test
   public void f() {
+	  Logger log=Logger.getLogger(NewTest.class.getClass());
+	  PropertyConfigurator.configure("log4j.properties");
 	  System.out.println("sample test executed");
-	  
+	  log.info("sample test");
   }
   @Test
   public void test2() {
+	  Logger log=Logger.getLogger(NewTest.class);
+	  PropertyConfigurator.configure("log4j.properties");
+	  
 int a=20;
 int b=30;
 Assert.assertEquals(50, a+b);
 System.out.println("Test case2 executed successfully in class1");  
-	  }
+log.info("sample calculation");  
+  }
 
 //something doing change
 }
